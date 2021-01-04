786751a9c41d790f465f70e744f6cc15793f4b1e
DawidR <DawidJavaScript@gmail.com>
2021-01-04 11:00:43 +0100

    95%

--- src/app/menu/t-shirt/t-shirt.component.ts
+++ src/app/menu/t-shirt/t-shirt.component.ts
@@ -6,15 +6,140 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./t-shirt.component.less'],
 })
 export class TShirtComponent implements OnInit {
-  number = 1;
+  public tShirts: TShirt[] = [
+    {
+      id: '1',
+      image: '../../../assets/shirt1.png',
+      name: 'T-SHIRT 1',
+      tips: [
+        {
+          side: 'left',
+          position: { x: -200, y: 70 },
+          text: 'aaaa kolniezyk',
+        },
+        {
+          side: 'right',
+          position: { x: 50, y: 200 },
+          text: 'zajebista lamowka',
+        },
+        {
+          side: 'right',
+          position: { x: 20, y: 400 },
+          text: 'brzydkin napis',
+        },
+      ],
+      description:
+        'Lorem1111, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam aspernatur qui possimus sapiente nobis libero ipsum eaque. Reiciendis iusto blanditiis non aut perspiciatis laboriosam voluptas voluptatem expedita itaque quidem!',
+    },
+    {
+      id: '2',
+      image: '../../../assets/shirt2.png',
+      name: 'T-SHIRT 2',
+
+      tips: [
+        {
+          side: 'left',
+          position: { x: -200, y: 70 },
+          text: 'super napis1',
+        },
+        {
+          side: 'right',
+          position: { x: 170, y: 400 },
+          text: 'ladny napis1',
+        },
+      ],
+      description:
+        'Lorem22222, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam aspernatur qui possimus sapiente nobis libero ipsum eaque. Reiciendis iusto blanditiis non aut perspiciatis laboriosam voluptas voluptatem expedita itaque quidem!',
+    },
+    {
+      id: '3',
+      image: '../../../assets/shirt.png',
+      name: 'T-SHIRT 3',
+      tips: [
+        {
+          side: 'right',
+          position: { x: 190, y: 250 },
+          text: 'napis super 3',
+        },
+      ],
+      description:
+        'Lorem2333333, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam aspernatur qui possimus sapiente nobis libero ipsum eaque. Reiciendis iusto blanditiis non aut perspiciatis laboriosam voluptas voluptatem expedita itaque quidem!',
+    },
+    {
+      id: '4',
+      image: '../../../assets/shirt1.png',
+      name: 'T-SHIRT 1',
+      tips: [
+        {
+          side: 'left',
+          position: { x: 20, y: 100 },
+          text: 'bawelniany kolniezyk',
+        },
+        {
+          side: 'right',
+          position: { x: 50, y: 200 },
+          text: 'zajebista lamowka',
+        },
+        {
+          side: 'right',
+          position: { x: 20, y: 400 },
+          text: 'brzydkin napis',
+        },
+      ],
+      description:
+        'Lorem1111, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam aspernatur qui possimus sapiente nobis libero ipsum eaque. Reiciendis iusto blanditiis non aut perspiciatis laboriosam voluptas voluptatem expedita itaque quidem!',
+    },
+    {
+      id: '5',
+      image: '../../../assets/shirt1.png',
+      name: 'T-SHIRT 1',
+      tips: [
+        {
+          side: 'left',
+          position: { x: 20, y: 100 },
+          text: 'bawelniany kolniezyk',
+        },
+        {
+          side: 'right',
+          position: { x: 50, y: 200 },
+          text: 'zajebista lamowka',
+        },
+        {
+          side: 'right',
+          position: { x: 20, y: 400 },
+          text: 'brzydkin napis',
+        },
+      ],
+      description:
+        'Lorem1111, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ullam aspernatur qui possimus sapiente nobis libero ipsum eaque. Reiciendis iusto blanditiis non aut perspiciatis laboriosam voluptas voluptatem expedita itaque quidem!',
+    }
+  ];
+
+  currentTShirt?: TShirt = this.tShirts[1];
 
   constructor() {}
 
   ngOnInit(): void {}
 
   // tslint:disable-next-line:typedef
-  choseNumber(btnNumber: number) {
-    this.number = btnNumber;
-    console.log(this.number);
+  choseTShirt(shirt: TShirt) {
+    this.currentTShirt = shirt;
+    console.log(shirt);
   }
+
+  trackByIndex = (index: number): number => index;
+}
+
+export interface TShirt {
+  id: string;
+  image: string;
+  name: string;
+  description: string;
+  tips?: Tip[];
+}
+
+export interface Tip {
+  position: { x: number; y: number };
+  text: string;
+  side: 'left' | 'right';
 }
